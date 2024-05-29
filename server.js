43862af07fd37608deb63c4b0cfccf301433f3af
Keith Spurr <keithspurr@ksqdesign.com>
2024-05-29 16:39:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ app.listen(port, () => {
     console.log('Server started!');
   });
 
-  app.route('/api/authorize').options((req,res,next)=>{
+  app.route('https://spot-auth.onrender.com/api/authorize').options((req,res,next)=>{
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
     next();
   });
-  app.route('/api/authorize').get((req, res) => {
+  app.route('https://spot-auth.onrender.com/api/authorize').get((req, res) => {
 
     var request = require('request'); // "Request" library
     var client_id = process.env.client_id; // Your client id
